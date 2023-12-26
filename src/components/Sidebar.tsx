48cecfee2ea2912f4f81e02c82b915d15e87b7cf
JadlionHD <48410066+JadlionHD@users.noreply.github.com>
2023-12-26 12:06:17 +0700

    fix lg:hidden for hamb icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,10 +11,7 @@ const Sidebar: FC<Props> = ({ children }) => {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col ml-4 mt-4">
           {/* Page content here */}
-          {/* <label htmlFor="my-drawer-2" className="btn btn-xs btn-primary drawer-button lg:hidden">
-            Open drawer
-          </label> */}
-          <label htmlFor="my-drawer-2" className="drawer-button w-8 mb-4">
+          <label htmlFor="my-drawer-2" className="drawer-button w-8 mb-4 lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
@@ -23,7 +20,7 @@ const Sidebar: FC<Props> = ({ children }) => {
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
               />
             </svg>
